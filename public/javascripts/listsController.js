1456a76f7f6b02341af2132d150ff7d4215d5aee
Luca Kiebel <github@luca-kiebel.de>
2018-02-08 17:49:07 +0100

    Removed code-duplication

--- public/javascripts/listsController.js
+++ public/javascripts/listsController.js
@@ -1,17 +1,23 @@
 var shoppingList = angular.module('ShoppingList', []);
 
+
+
 function mainController($scope, $http) {
 	$scope.formData = {};
 
-	// when landing on the page, get all todos and show them
-	$http.get('/api/items/'+listId)
-		.success(function(data) {
-			$scope.items = data.items;
-			console.log(data);
-		})
-		.error(function(data) {
-			console.log('Error: ' + data);
-		});
+	$scope.getItems = function () {
+		$http.get('/api/items/'+listId)
+				.success(function(data) {
+					$scope.items = data.items;
+					console.log(data);
+				})
+				.error(function(data) {
+					console.log('Error: ' + data);
+				});
+	};
+
+	// when landing on the page, get all items and show them
+	$scope.getItems();
 
 	// when submitting the add form, send the text to the node API
 	$scope.createTodo = function() {
@@ -20,14 +26,7 @@ function mainController($scope, $http) {
 			$http.post('/api/items', $scope.formData)
 				.success(function(data) {
 					$scope.formData = {}; // clear the form so our user is ready to enter another
-					$http.get('/api/items/'+listId)
-						.success(function(data) {
-							$scope.items = data.items;
-							console.log(data);
-						})
-						.error(function(data) {
-							console.log('Error: ' + data);
-						});
+					$scope.getItems();
 				})
 				.error(function(data) {
 					console.log('Error: ' + data);
@@ -38,18 +37,11 @@ function mainController($scope, $http) {
 		}
 	};
 
-	// delete a todo after checking it
+	// delete an item after checking it
 	$scope.deleteTodo = function(id) {
 		$http.delete('/api/items/' + id)
 			.success(function(data) {
-				$http.get('/api/items/' + listId)
-					.success(function(data) {
-						$scope.items = data.items;
-						console.log(data);
-					})
-					.error(function(data) {
-						console.log('Error: ' + data);
-					});
+				$scope.getItems();
 			})
 			.error(function(data) {
 				console.log('Error: ' + data);

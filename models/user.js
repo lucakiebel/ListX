b0f9c0e88050b18c475e30848bd81441acc78215
Luca Kiebel <github@luca-kiebel.de>
2018-02-09 20:01:33 +0100

    Add actuallyRemoveDo function, that handles the remove after sending mail

--- models/user.js
+++ models/user.js
@@ -64,13 +64,38 @@ UserSchema.statics.getListsByQuery = function (id, query, callback) {
   });
 };
 
-UserSchema.statics.actuallyRemove = function (id, callback) {
+UserSchema.validate = function (id, ) {
+    
+}
+
+UserSchema.statics.actuallyRemoveCreate = function (id, callback) {
   mongoose.model("UserDeletionToken").create({userId: id}, function (err, token) {
+    err && callback(err);
     token = token._id;
     this.findOne({_id: id}, function (err, user) {
-
+      err && callback(err);
+      if (user) {
+          let URL = `https://${config.domain}/user/delete/${token}`;
+          let mailData = {
+              to: user.email,
+              subject: "ListX Account Deletion",
+              body: `Hey ${user.name}, \nYou (or someone else) just requested deletion for this ListX account. \n\nIf it was you and you are trying to delete your account, please follow this link in order to do so: \n\t${URL} \n\nIf you did not request account deletion, please immediately change your password, it might be known to someone else. \n\nListX Support`,
+              send: true
+          };
+          callback(false, mailData);
+      }
     })
   })
+};
+
+UserSchema.statics.actuallyRemoveDo = function (id, callback) {
+    mongoose.model("UserDeletionToken").findOne({_id: id}, function (err, token) {
+        err && callback(err);
+        this.remove({_id: token.userId}, function (err, user) {
+            err && callback(err);
+            callback(false, user, {success: true});
+        });
+    });
 };
 
 module.exports = mongoose.model('User', UserSchema);

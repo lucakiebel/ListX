b9c16a06d4f04d873dde9e8918f93bdf72710279
Luca Kiebel <github@luca-kiebel.de>
2018-02-11 17:05:55 +0100

    Add functions for Name and Country Change of lists

--- public/javascripts/list-settings-admin.js
+++ public/javascripts/list-settings-admin.js
@@ -47,4 +47,51 @@ $(document).ready(function() {
      * TODO: Delete Invitations (delete)
      * TODO: Remove Users (modal => confirmation, delete)
      */
+
+    $("#name-change-button").click(() => {
+        let $nameInput = $("#list-name");
+        $nameInput.prop("disabled", false);
+        $nameInput.keypress(e => {
+            if (e.which === 13) {
+                e.preventDefault();
+                if (listAdmin !== userId) {
+                    $("#save-error").show();
+                } else {
+                    $.post("/api/lists/update/name",
+                        {list:listId, newName:$nameInput.val(), admin:userId}, data => {
+                        if (data.success) {// reload page
+                            window.location.reload(true);
+                        } else {
+                            $("#save-error").show();
+                        }
+                    });
+                }
+            }
+        });
+    });
+
+    $("#country-change-button").click(() => {
+        let $countryInput = $("#list-country");
+        $countryInput.prop("disabled", false);
+        $countryInput.keypress(e => {
+            if (e.which === 13) {
+                e.preventDefault();
+                if (listAdmin !== userId) {
+                    $("#save-error").show();
+                } else {
+                    $.post("/api/lists/update/country",
+                        {list:listId, newCountry:$countryInput.val(), admin:userId}, data => {
+                        if (data.success) {//reload page
+                            window.location.reload(true);
+                        } else {
+                            $("#save-error").show();
+                        }
+                    })
+                }
+            }
+        })
+    });
+
+
+
 });

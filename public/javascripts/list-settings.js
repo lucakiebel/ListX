12f1ed3565b43e73dfdeb4b2ff46f0266799c666
Luca Kiebel <github@luca-kiebel.de>
2018-02-12 14:11:35 +0100

    Add all neccessary functionality to the normal user's list settings

--- public/javascripts/list-settings.js
+++ public/javascripts/list-settings.js
@@ -12,15 +12,65 @@ $(document).ready(function() {
         });
 
 
-    /**
-     * TODO: Name and Country Changer
-     * TODO: [Current Users/Current Invitations]:
-     *      beforeItemRemove (check for admin)
-     *      itemRemoved (remove user)
-     *      beforeItemAdd (sanity check [duplicate])
-     *      itemAdded (add user)
-     * TODO: Delete List (modal => confirmation, delete)
-     * TODO: Delete Invitations (delete)
-     * TODO: Remove Users (modal => confirmation, delete)
-     */
+    let listAdmin; // "objectid"
+    let currentUsers; // [{email:"String", _id:"objectid"}]
+    let currentInvitations; // [{email:"String", _id:"objectid"}]
+    let currentlyInSetup = false;
+
+    //get admin
+    $.get("/api/lists/"+listId, data => {
+        if (data.admin) {
+            $.get("/api/users/"+data.admin, data => {
+                listAdmin = data._id.toString();
+                $("#list-admin").val(data.email);
+            })
+        }
+    });
+
+    //get current users
+    $.get("/api/lists/"+listId+"/userEmails", data => {
+        if (data.users && data.users.length !== 0 && data.success) { // sanity, should never return []. (admin)
+            console.log("Current Users:", data.users);
+            $("#current-users").tagsinput('removeAll');
+            currentUsers = data.users;
+            currentlyInSetup = true; // we want to be able to add users
+            data.users.forEach(u => {
+                u.email && $("#current-users").tagsinput('add', u.email);
+            });
+            currentlyInSetup = false;
+        }
+    });
+
+    //get current invitations
+    $.get("/api/lists/"+listId+"/invitationsForSettings", data => {
+        if (data.success) {
+            console.log("Current Invitations:", data.invitations);
+            $("#current-invitations").tagsinput("removeAll");
+            currentInvitations = data.invitations;
+            currentlyInSetup = true; // we don't want to re-invite everyone onload
+            data.invitations.forEach(i => {
+                i.email && $("#current-invitations").tagsinput("add", i.email);
+            });
+            currentlyInSetup = false;
+        }
+    });
+
+    $('#current-users')
+        .on('beforeItemAdd', function(event) {
+            event.cancel = !currentlyInSetup;
+        });
+    $("#current-invitations")
+        .on("itemAdded", function (event) {
+            event.cancel = !currentlyInSetup;
+        });
+
+    $("#leaveListBtn").click(function () {
+       if (window.confirm("Do you really want to leave this list? You will have to be re-invited by the admin.")) {
+           $.post("/api/lists/"+listId+"/removeMeFromList", {"user":userId}, data => {
+               if (data.success) {
+                   window.location.replace("/dashboard");
+               }
+           })
+       }
+    });
 });

dcc25af206a3bc6539d4e128ccfaba702d9f8d8e
Luca Kiebel <github@luca-kiebel.de>
2018-03-05 21:49:20 +0100

    no message

--- app.js
+++ app.js
@@ -1597,6 +1597,7 @@ function validateReCAPTCHA(gResponse, callback) {
 		`https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${gResponse}`,
 		function (error, response, body) {
 			body = JSON.parse(body);
+			console.log(body);
 			callback(null, body.success);
 		}
 	);

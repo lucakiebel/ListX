9289ee551723a88b746d8ee256d5596bda4fbf89
Luca Kiebel <github@luca-kiebel.de>
2018-03-07 22:11:15 +0100

    deleting an invitation from a list now requires that the admin does so

--- app.js
+++ app.js
@@ -1510,9 +1510,9 @@ app.delete('/api/invitations/list/:id', requireAuthentication, (req, res) => {
 });
 
 // Delete invitation for user from list /list/ admin, user
-app.post("/api/invitations/user/list/:list", (req, res) => {
+app.post("/api/invitations/user/list/:list", requireAuthentication, (req, res) => {
 	List.findOne({_id: req.params.list}, (err, list) => {
-		if (list.admin.toString() === req.body.admin.toString()) {
+		if (list.admin.toString() === req.authentication.user._id.toString()) {
 			Invitation.findOneAndRemove({email: req.body.user, list: list._id}, (err, removed) => {
 				!!err && res.json({success: false, err: err});
 				res.json({success: true, user: removed._id});
